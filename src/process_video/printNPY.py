a0eaeab4ee82ba62812f15ffce4140aea0cc85e9
西伯利亚大土豆 <1260855411@qq.com>
2025-11-16 17:06:49 +0800

    he数据集

--- src/process_video/printNPY.py
+++ src/process_video/printNPY.py
@@ -3,6 +3,6 @@ import numpy as np
 # 设置numpy打印选项，取消截断
 np.set_printoptions(threshold=np.inf)
 
-data = np.load('../../data/processed/video/repeat_video_01/action_2_features.npy')
+data = np.load('../../data/dataset/train/he/02/he_1_features.npy')
 print(data.shape)
 print(data)
--- src/process_video/process_repeat_action_video.py
+++ src/process_video/process_repeat_action_video.py
@@ -68,7 +68,7 @@ class HandFeatureExtractor:
 
     def detect_action_boundaries_repeat(self, trajectory,
                                             min_action_len=5,
-                                            max_pause_len=20):
+                                            max_pause_len=27):
         """
         允许动作中有短暂停留，避免将一个动作拆成多个
         """
@@ -210,8 +210,8 @@ class HandFeatureExtractor:
 # ----------------------------
 # 使用示例
 # ----------------------------
-video_file = "../../data/raw/video/repeat_action_you_01.mp4"
-output_folder = "../../data/processed/video/repeat_video_01"
+video_file = "../../data/raw/video/repeat_he_06.mp4"
+output_folder = "../../data/dataset/train/he/06"
 os.makedirs(output_folder, exist_ok=True)
 
 extractor = HandFeatureExtractor()
@@ -276,7 +276,7 @@ def extract_action_features(video_path, boundaries, output_dir, fixed_frame_coun
         all_action_features.append(features)
 
         # 保存每个动作
-        save_path = os.path.join(output_dir, f"action_{idx+1}_features.npy")
+        save_path = os.path.join(output_dir, f"he_{idx+1}_features.npy")
         np.save(save_path, features)
         print(f"动作 {idx+1} 特征 shape: {features.shape}, 保存到: {save_path}")
 
